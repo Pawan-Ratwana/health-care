2e4ca11ed2e18688874157e8b8451a1364ad3aca
Pawan Kumar <ratwanapawan@gmail.com>
2024-01-13 09:33:10 +0530

    basic setup of express and connect to mongoose

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+const db = require('./config/mongoose')
+
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello</h1>")
+})
+
+
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(`Server is running on http://localhost:${port}`)
+    }
+})
